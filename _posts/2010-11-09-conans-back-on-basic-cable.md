2533235699d135b72b43b68212a61f71df41f17c
Brian Middleton <github@centerweight.com>
2013-05-14 07:44:46 -0700

    Initial Commit

---
+++ _posts/2010-11-09-conans-back-on-basic-cable.md
@@ -0,0 +1,13 @@
+---
+layout: post
+title: Conan's Back on Basic Cable
+---
+
+My decision to cut cable tv wasn't exactly a hard one. Most of the shows I cared about were either available online or over-the-air. It wasn't until recently I got a slight twinge of regret. That was when Conan moved his new show to TBS.
+
+<div class="image-block">
+  <a href="http://www.tbs.com/video/conan/"><img src="http://www.centerweight.com/wordpress/wp-content/uploads/2010/11/img_pale_whale.jpg" alt="" /></a>
+</div>
+
+
+Luckily, <a href="http://www.teamcoco.com">teamcoco.com</a> has come to the rescue and is posting new episodes the day after they air. Crisis averted. <a href="http://www.tbs.com/video/conan/">Watch now »</a>
---
+++ _site/javascript/scripts.js
@@ -0,0 +1,30 @@
+  $(document).ready(function () {
+
+  // Please use your own API key !
+  $('.lastfm-chart').lastfm('aapljack','topalbums','e0de46d270197ba1e7878e99a5b54085', {
+    period: '7day',
+    limit: '5',
+    image: false,
+    header: false
+  });
+  $(".instagram").instagram({
+    userId: '1588265',
+    clientId: 'e0d748e676a04859b7bebb33a1cb91eb',
+    accessToken: '1588265.e0d748e.9c91e51e830d41b9ae88296b659fdb18',
+    show: '12',
+    image_size: 'low_resolution'
+  });
+});
+
+
+// curl \-F 'client_id=e0d748e676a04859b7bebb33a1cb91eb' \
+//     -F 'client_secret=4267c0a987ce4e16a86c89b75f872f53' \
+//     -F 'grant_type=authorization_code' \
+//     -F 'redirect_uri=http://www.centerweight.com' \
+//     -F 'code=bc8b5fbca7104b30a57fd30d7318442f' \https://api.instagram.com/oauth/access_token
+
+
+  //     https://api.instagram.com/oauth/authorize/?client_id=e0d748e676a04859b7bebb33a1cb91eb&redirect_uri=http://www.centerweight.com&response_type=code
+
+
+  //     Return Code = bc8b5fbca7104b30a57fd30d7318442f
