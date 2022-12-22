9e09641ddc8d832e09f23da24461d3429e25fe7e
Brian Middleton <brian@planetargon.com>
2022-12-22 15:31:53 -0800

    Revised the period for the lastfm plugin. Updated the number of returned results for the pinboard plugin.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   $('.lastfm-chart').lastfm('aapljack','topalbums','e0de46d270197ba1e7878e99a5b54085', {
-    period: '7day',
+    period: '1month',
     limit: '5',
     image: false,
     header: false
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
   $("#elsewhere").FeedEk({
     FeedUrl : 'https://feeds.pinboard.in/rss/u:aapljack/t:centerweight',
-    MaxCount : 3,
+    MaxCount : 4,
     ShowDesc : true,
     ShowPubDate: false,
     TitleLinkTarget:'_blank'
